144813167f3ae14b8c0b03531a008cd5424a8e7a
Felipe Vogt <felipevogt.f@gmail.com>
2023-11-22 00:46:59 -0300

    javascript component

--- src/app/pages/code/javascript/javascript.component.ts
+++ src/app/pages/code/javascript/javascript.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Subject, debounceTime } from 'rxjs';
 
 @Component({
@@ -7,103 +8,156 @@ import { Subject, debounceTime } from 'rxjs';
   styleUrl: './javascript.component.scss',
 })
 export class JavascriptComponent {
-  editorOptions = { theme: 'vs-dark', language: 'javascript' };
+  editorOptions = {
+    theme: 'vs-dark',
+    language: 'javascript',
+    fontSize: '16px',
+    lineHeight: '21px',
+    fontFamily: 'Cascadia Code, system-ui',
+    fontLigatures: true,
+  };
   code: string = '';
   output: string = '';
+  outputHTML: SafeHtml = '';
+  loading: boolean = true;
+  running: boolean = false;
   private codeEvent: Subject<string> = new Subject<string>();
 
-  constructor() {
+  constructor(
+    private sanitizer: DomSanitizer,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
     const originalLog = console.log;
     const originalError = console.error;
     const originalWarn = console.warn;
-    const originalTable = console.table;
 
     console.log = (...args) => {
-      this.output += args.join(' ') + '\n';
+      this.output += this.logToHTML(args.join(' ')) + '\n';
       originalLog.apply(console, args);
     };
 
     console.error = (...args) => {
-      this.output += args.join(' ') + '\n';
+      this.output += this.errorToHTML(args.join(' ')) + '\n';
       originalError.apply(console, args);
     };
 
     console.warn = (...args) => {
-      this.output += args.join(' ') + '\n';
+      this.output += this.warnToHTML(args.join(' ')) + '\n';
       originalWarn.apply(console, args);
     };
 
-    console.table = (...args) => {
-      args.forEach((table) => {
-        if (Array.isArray(table)) {
-          this.output += this.arrayToHTMLTable(table) + '<br>';
-        } else if (typeof table === 'object') {
-          this.output += this.objectToHTMLTable(table) + '<br>';
-        }
-      });
-      originalTable.apply(console, args);
-    };
-
     this.codeEvent.pipe(debounceTime(1000)).subscribe((value) => {
-      this.output = '';
-      console.clear();
-
-      console.log = (...args) => {
-        this.output += args.join(' ') + '\n';
-        originalLog.apply(console, args);
-      };
-
-      try {
-        const executionFunction = new Function(value);
-        const result = executionFunction();
-
-        if (result) {
-          this.output += result;
-        }
-      } catch (error: any) {
-        this.output += error;
-      }
+      this.play();
     });
   }
 
-  ngOnInit() {
-    // monaco.editor.create(document.getElementById('editor')!, {
-    //   value: 'function hello() {\n\talert("Hello, world!");\n}',
-    //   language: 'html',
-    //   theme: 'vs-dark',
-    // });
-  }
+  ngOnInit() {}
 
   executeCode() {
     this.codeEvent.next(this.code);
   }
 
-  arrayToHTMLTable(data: any[]) {
-    let html = '<table border="1"><tr>';
-    const headers = Object.keys(data[0]);
-    headers.forEach((header) => {
-      html += `<th>${header}</th>`;
-    });
-    html += '</tr>';
-    data.forEach((item) => {
-      html += '<tr>';
-      headers.forEach((header) => {
-        html += `<td>${item[header]}</td>`;
-      });
-      html += '</tr>';
-    });
-    html += '</table>';
-    return html;
+  clear() {
+    this.running = true;
+    this.output = '';
+    this.outputHTML = '';
   }
 
-  objectToHTMLTable(data: any) {
-    let html = '<table border="1">';
-    for (const key in data) {
-      if (data.hasOwnProperty(key)) {
-        html += `<tr><td>${key}</td><td>${data[key]}</td></tr>`;
-      }
+  async play() {
+    if (this.running) {
+      return;
     }
-    html += '</table>';
-    return html;
+    
+    this.clear();
+
+    setTimeout(() => {
+      const result = this.execute(this.code);
+      this.output += result;
+
+      this.outputHTML = this.sanitizerHTML(this.output);
+      this.running = false;
+    }, 300);
+  }
+
+  execute(value: string): string {
+    let data = '';
+    try {
+      const executionFunction = new Function(value);
+      const result = executionFunction();
+
+      if (result) {
+        data = `<p>${result}</p>` + '\n';
+      }
+    } catch (error: any) {
+      data = this.errorToHTML(error) + '\n';
+    }
+
+    return data;
+  }
+
+  logToHTML(value: string) {
+    return `
+    <p
+      style="
+        display: flex;
+        flex-direction: row;
+        column-gap: 20px;
+        padding: 20px;
+        background-color: var(--color-light);
+        color: var(--color-light-contrast);
+        border-radius: var(--base-radius);
+      "
+    >
+      <i class="fa-solid fa-chevron-right"></i>
+      <span>${value}</span>
+    </p>
+    `;
+  }
+
+  errorToHTML(value: string) {
+    return `
+    <p
+      style="
+        display: flex;
+        flex-direction: row;
+        column-gap: 20px;
+        padding: 20px;
+        background-color: var(--color-danger);
+        color: var(--color-danger-contrast);
+        border-radius: var(--base-radius);
+      "
+    >
+      <i class="fa-solid fa-xmark"></i>
+      <span>${value}</span>
+    </p>
+    `;
+  }
+
+  warnToHTML(value: string) {
+    return `
+    <p
+      style="
+        display: flex;
+        flex-direction: row;
+        column-gap: 20px;
+        padding: 20px;
+        background-color: var(--color-warning);
+        color: var(--color-warning-contrast);
+        border-radius: var(--base-radius);
+      "
+    >
+      <i class="fa-solid fa-triangle-exclamation"></i>
+      <span>${value}</span>
+    </p>
+    `;
+  }
+
+  sanitizerHTML(value: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(value);
+  }
+
+  onMonacoEditorInit(event: any): void {
+    this.loading = false;
+    this.changeDetectorRef.detectChanges();
   }
 }
