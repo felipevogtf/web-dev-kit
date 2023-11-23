874441cc22ccbafc5699e2b0d254694c3ba11184
Felipe Vogt <felipevogt.f@gmail.com>
2023-11-23 17:27:17 -0300

    ajuste menus

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  state,
+} from '@angular/animations';
 
 @Component({
   selector: 'nav-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.scss',
+  animations: [
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ height: 0, opacity: 0 }),
+        animate('150ms ease-in', style({ height: '*', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('150ms ease-out', style({ height: 0, opacity: 0 })),
+      ]),
+    ]),
+
+    trigger('openClose', [
+      state('true', style({ height: '*' })),
+      state('false', style({ height: '0px' })),
+      transition('false <=> true', animate(500)),
+    ]),
+  ],
 })
 export class NavMenuComponent {
   isToggle: boolean = false;
@@ -70,25 +94,53 @@ export class NavMenuComponent {
     },
   ];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.checkRoute();
+      }
+    });
+  }
 
   toggle() {
     this.isToggle = !this.isToggle;
   }
 
-  test(menu: any) {
-    if (menu.childs) {
-      let menusToggles = this.menus.filter((item) => item.toggle);
-      menu.toggle = !menu.toggle;
-
+  toggleLink(menu: any) {
+    const closeMenus = () => {
+      let menusToggles = this.menus.filter(
+        (item) => item.toggle && item != menu
+      );
       for (const item of menusToggles) {
         item.toggle = false;
       }
-      return;
-    }
+    };
 
-    if (menu.link) {
-      this.router.navigate([menu.link]);
-    }
+    closeMenus();
+    menu.toggle = !menu.toggle;
+  }
+
+  checkRoute() {
+    const closeMenus = () => {
+      let menusToggles = this.menus.filter((item) => item.toggle);
+      for (const item of menusToggles) {
+        item.toggle = false;
+      }
+    };
+
+    closeMenus();
+    
+    const routeSearch = this.router.url;
+    const filterMenus = this.menus.filter((menu) => {
+      if (menu.link === routeSearch) {
+        return true;
+      }
+      if (menu.childs) {
+        return menu.childs.some((child) => child.link === routeSearch);
+      }
+      return false;
+    });
+    const menu = filterMenus[0];
+    menu.toggle = true;
   }
 }
