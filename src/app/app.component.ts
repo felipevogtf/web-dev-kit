4df24783e08f8d75cf7b07ee5232aec0781f6063
Felipe Vogt <felipevogt.f@gmail.com>
2023-11-28 13:19:02 -0300

    markdown component y ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component, HostListener, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavMenuComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -14,14 +13,5 @@ export class AppComponent {
   title = 'web-dev-kit';
   resizeTimer: any;
 
-  constructor(private renderer: Renderer2) {}
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.renderer.addClass(document.body, 'resize-animation-stopper');
-    clearTimeout(this.resizeTimer);
-    this.resizeTimer = setTimeout(() => {
-      this.renderer.removeClass(document.body, 'resize-animation-stopper');
-    }, 200);
-  }
+  constructor() {}
 }
