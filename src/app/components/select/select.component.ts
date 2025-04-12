5c8345fe2a9ff95f42ee6b18c3a0aae696ae35ba
Felipe Vogt <felipevogt.f@gmail.com>
2025-04-12 13:21:56 -0400

    feat: Implement color picker and UUID generator components with styling and tests

---
+++ src/app/components/select/select.component.ts
@@ -0,0 +1,64 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { LabelComponent } from '@components/label/label.component';
+
+@Component({
+  selector: 'wdk-select',
+  standalone: true,
+  imports: [
+    CommonModule,
+    LabelComponent,
+    FormsModule
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true,
+    },
+  ],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss'
+})
+export class SelectComponent implements ControlValueAccessor{
+  @Input() id!: string;
+  @Input() label: string;
+  @Input() readonly: boolean;
+  @Input() disabled: boolean;
+
+  value: string = '';
+
+  constructor() {
+    this.label = '';
+    this.disabled = false;
+    this.readonly = false;
+  }
+
+  // Métodos requeridos por ControlValueAccessor
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    // Lógica para establecer el estado deshabilitado si es necesario
+  }
+
+  onInputChange(event: Event) {
+    const newValue = (event.target as HTMLInputElement).value;
+    this.value = newValue;
+    this.onChange(newValue);
+    this.onTouched();
+  }
+}
